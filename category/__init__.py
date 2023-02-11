433f3249c850975100f4a751ec50c674437f1f9c
Nikola N <kolzazanovikov@gmail.com>
2023-02-11 18:00:18 +0800

    fix init

--- category/__init__.py
+++ category/__init__.py
@@ -1,6 +1,6 @@
 from .category import Category
 from .repo import CategoryRepo
-from .model import CategoryModel, CustomButtonsModel
+from .model import CategoryModel, CustomButtonsModel, MessageTextModel
 from .buttons import CategoryButtons
 
-__all__=(Category, CategoryRepo, CategoryModel, CustomButtonsModel, CategoryButtons)
+__all__=(Category, CategoryRepo, CategoryModel, MessageTextModel, CustomButtonsModel, CategoryButtons)
