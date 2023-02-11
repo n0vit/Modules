34e332fa724f8c02db25ea555bc5e02312a3128e
Nikola N <kolzazanovikov@gmail.com>
2023-02-11 15:54:17 +0800

    update

--- category/category.py
+++ category/category.py
@@ -1,4 +1,5 @@
 from typing import List
+
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
--- chain/__init__.py
+++
@@ -1,8 +0,0 @@
-from .messages_chain import MessagesChain, MessageChainStates
-from .chain_model import ChainModel
-
-__all__ = (MessagesChain,MessageChainStates,ChainModel)
-
-from aiogram import __version__ as AIOGRAM_VERSION
-
-AIOGRAM_VERSION = int(AIOGRAM_VERSION[0])
