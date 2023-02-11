70da864c62f657d07e0293d7eaf0defceccae78f
Nikola N <kolzazanovikov@gmail.com>
2023-02-11 16:06:14 +0800

    o

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .category.category import Category
-from .category.repo import CategoryRepo
-from .category.model import CategoryModel, CustomButtonsModel
-from .category.buttons import CategoryButtons
-
-__all__=(Category, CategoryRepo, CategoryModel, CustomButtonsModel, CategoryButtons)
